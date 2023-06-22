c8be0e82145a7afbbf562a86cc8dbc1ea3543f3f
gabrielc42 <clark.gabe97@gmail.com>
2023-06-22 12:55:37 -0500

    postsController, create new post button, idiosyncratic formatting & code organization

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -7,7 +7,10 @@
             <img src="/svg/smiley-alt-svgrepo-com.svg" alt="smiley face enclosed in square, enclosed in circle" style="height: 10rem;">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>{{ $user -> username }}</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user -> username }}</h1>
+                <a href="">Add New Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-3" style="padding: 1rem"><strong>153</strong> posts</div>
                 <div class="pr-3" style="padding: 1rem"><strong>23k</strong> followers</div>
