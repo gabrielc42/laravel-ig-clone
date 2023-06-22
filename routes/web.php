2ec5e8b7495f879add89f73d6094b70728836aae
gabrielc42 <clark.gabe97@gmail.com>
2023-06-22 13:30:26 -0500

    add new post caption/image verification

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/p/create', 'PostsController@create');
+Route::get('/p/create', [App\Http\Controllers\PostsController::class, 'create']);
+Route::post('/p', [App\Http\Controllers\PostsController::class, 'store']);
+
 
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
