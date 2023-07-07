30e0b1c50a667d9c0305cfa9a57b275a150f989f
gabrielc42 <clark.gabe97@gmail.com>
2023-07-07 16:09:53 -0500

    restricting/auth actions with model policy

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,9 +9,16 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
                 <h1>{{ $user -> username }}</h1>
-                <a href="/p/create">Add New Post</a>
+
+                @can('update', $user->profile)
+                    <a href="/p/create">Add New Post</a>
+                @endcan
+
             </div>
-            <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            
+            @can('update', $user->profile)
+                <a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
+            @endcan
 
             <div class="d-flex">
                 <div class="pr-3" style="padding: 1rem"><strong>{{ $user->posts->count() }}</strong> posts</div>
